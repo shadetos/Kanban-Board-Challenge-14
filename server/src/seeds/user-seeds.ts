14817af7a610df477c301cc75ea0e786b75ca13d
R. Shadetos <shadetos@gmail.com>
2025-03-14 03:06:34 +0000

    updated seeds

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -2,8 +2,8 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'JollyGuru', password: 'password' },
-    { username: 'SunnyScribe', password: 'password' },
-    { username: 'RadiantComet', password: 'password' },
+    { username: 'WestWood', password: 'password' },
+    { username: 'SunnySideUp', password: 'password' },
+    { username: 'WhatWhat', password: 'password' },
   ], { individualHooks: true });
 };
